100d8631913a481226e9bb0de9b48a8b51b8aebc
k anand <kanandaiah9@gmail.com>
2023-07-20 20:58:07 +0530

    all scrprits

--- mysql.sh
+++ mysql.sh
@@ -6,8 +6,6 @@ echo -e "\e[32m <<<<<<<< install mysl community>>>>>>>>>>\e[0m"
 yum install mysql-community-server -y
 echo -e "\e[32m <<<<<<<< enable and start mysql>>>>>>>>>>\e[0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 echo -e "\e[32m <<<<<<<< set root passwd>>>>>>>>>>\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
-echo -e "\e[32m <<<<<<<< u root passwd>>>>>>>>>>\e[0m"
-mysql -uroot -pRoboShop@1
