850a2afccb7ad69165c4deebb3074cd9d29155ff
k anand <kanandaiah9@gmail.com>
2023-07-20 14:47:53 +0530

    all scrprits

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,30 @@
+echo -e "\e[32m <<<<<<<< configure nodejs >>>>>>>>>>\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[32m <<<<<<<< install nodejs >>>>>>>>>>\e[0m"
 yum install nodejs -y
+echo -e "\e[32m <<<<<<<< add roboshop >>>>>>>>>>\e[0m"
 useradd roboshop
+echo -e "\e[32m <<<<<<<< create directory >>>>>>>>>>\e[0m"
+rm -rf /app
 mkdir /app
+echo -e "\e[32m <<<<<<<< catalogue zip file >>>>>>>>>>\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo -e "\e[32m <<<<<<<< inside the dirctory  >>>>>>>>>>\e[0m"
 cd /app
+echo -e "\e[32m <<<<<<<< extract ip file >>>>>>>>>>\e[0m"
 unzip /tmp/catalogue.zip
+echo -e "\e[32m <<<<<<<< install npm >>>>>>>>>>\e[0m"
 npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[32m <<<<<<<< copy the catalogue service file >>>>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[32m <<<<<<<< daemon reload >>>>>>>>>>\e[0m"
 systemctl daemon-reload
+echo -e "\e[32m <<<<<<<<enable and start service >>>>>>>>>>\e[0m"
 systemctl enable catalogue
-systemctl start catalogue
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+systemctl restart catalogue
+echo -e "\e[32m <<<<<<<< copy repo files >>>>>>>>>>\e[0m"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[32m <<<<<<<< install mongodb >>>>>>>>>>\e[0m"
 yum install mongodb-org-shell -y
+echo -e "\e[32m <<<<<<<< load mongodb schema >>>>>>>>>>\e[0m"
 mongo --host mongodb-dev.kanand.online </app/schema/catalogue.js
