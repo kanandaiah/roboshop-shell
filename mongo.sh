51da8c869b1603c849785dfa7009ed60b3ddd5ee
k anand <kanandaiah9@gmail.com>
2023-07-20 11:55:50 +0530

    all scrprits

---
+++ mongo.sh
@@ -0,0 +1,5 @@
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl restart mongod
+#Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
